adff5e9942c32e6364b39a61871f24366d7bf289
Albert Albala <albert@mediatribe.net>
2024-10-24 14:53:32 -0400

    Support Drupal 11 (#15)

--- scripts/php-lint.sh
+++ scripts/php-lint.sh
@@ -4,13 +4,14 @@
 #
 set -e
 
-echo '=> Linting PHP code.'
+echo 'Linting PHP files'
 echo 'If you are getting a false negative, use:'
+echo 'Linting PHP files with https://github.com/dcycle/docker-php-lint'
+echo 'If you are getting a false positive, use:'
 echo ''
 echo '// @codingStandardsIgnoreStart'
 echo '...'
 echo '// @codingStandardsIgnoreEnd'
-echo ''
 echo ''
 echo 'To automatically fix errors, you can run:'
 echo ''
